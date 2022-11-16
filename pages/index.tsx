8ed21a1f419cd6ac16daf1e8cecb894feb238899
wilsonbljr <wilsonbljr@gmail.com>
2022-11-16 18:34:57 -0300

    feat(favicon): implements favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,24 @@ export default function Home() {
   return (
     <>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <title>Wilson Bley</title>
       </Head>
       <Header />
