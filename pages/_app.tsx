45105539add4163b185d407d623b79afcd04fb48
wilsonbljr <wilsonbljr@gmail.com>
2022-12-02 22:05:17 -0300

    feat(analytics): adds google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { NextIntlProvider } from "next-intl";
 import { Roboto } from "@next/font/google";
 import Footer from "../components/Footer";
+import Script from "next/script";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -12,13 +13,28 @@ const roboto = Roboto({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <main
-        className={`${roboto.variable} font-sans min-h-screen bg-slate-800 text-gray-200 relative`}
-      >
-        <Component {...pageProps} />
-        <Footer />
-      </main>
-    </NextIntlProvider>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.TAG}`}
+      ></Script>
+      <Script id="google-tag-manager">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', ${process.env.TAG});
+        `}
+      </Script>
+      <NextIntlProvider messages={pageProps.messages}>
+        <main
+          className={`${roboto.variable} font-sans min-h-screen bg-slate-800 text-gray-200 relative`}
+        >
+          <Component {...pageProps} />
+          <Footer />
+        </main>
+      </NextIntlProvider>
+    </>
   );
 }
